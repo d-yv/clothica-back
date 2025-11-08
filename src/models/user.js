c321ad8dbf27017082bfa9e2261a989072bd2536
d-yv <yuri.dubrovin@gmail.com>
2025-11-08 17:47:13 +0200

    fix errors

--- src/models/user.js
+++ src/models/user.js
@@ -2,27 +2,39 @@ import { model, Schema } from 'mongoose';
 
 const userSchema = new Schema(
   {
-    username: {
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
       type: String,
       trim: true,
     },
     phone: {
       type: String,
+      trim: true,
       unique: true,
       required: true,
-      trim: true,
     },
     password: {
       type: String,
       required: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+    },
+    postOfficeNum: {
+      type: String,
+      trim: true,
     },
   },
   { timestamps: true, versionKey: false },
 );
 
 userSchema.pre('save', function (next) {
-  if (!this.username) {
-    this.username = this.phone;
+  if (!this.firstName) {
+    this.firstName = this.phone;
   }
   next();
 });
--- src/routes/goodsRoutes.js
+++ src/routes/goodsRoutes.js
@@ -1,11 +1,14 @@
-import { Router } from "express";
-import { celebrate } from "celebrate";
-import { getAllGoodsSchema, goodIdSchema } from "../validations/goodsValidation.js";
-import { getAllGoods, getGoodById } from "../controllers/goodsController.js";
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import {
+  getAllGoodsSchema,
+  goodIdSchema,
+} from '../validation/goodsValidation.js';
+import { getAllGoods, getGoodById } from '../controllers/goodsController.js';
 
 const router = Router();
 
-router.get("/goods", celebrate(getAllGoodsSchema), getAllGoods);
-router.get("/goods/:goodId", celebrate(goodIdSchema), getGoodById);
+router.get('/goods', celebrate(getAllGoodsSchema), getAllGoods);
+router.get('/goods/:goodId', celebrate(goodIdSchema), getGoodById);
 
 export default router;
