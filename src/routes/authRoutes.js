e1908794e743bb63a89f78cba7a2eded305a9285
ilkash <126604951+ilkash@users.noreply.github.com>
2025-11-12 18:24:10 +0200

    api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -15,8 +15,8 @@ import {
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), registerUser);
-router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-router.post('/auth/logout', logoutUser);
-router.post('/auth/refresh', refreshUserSession);
+router.post('/api/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/api/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/api/auth/logout', logoutUser);
+router.post('/api/auth/refresh', refreshUserSession);
 export default router;
